af0d194ac507a0e1529cbbb9af85d632ffac0f4d
zhenfei <zhenfei.zhang@hotmail.com>
2021-05-17 06:46:23 -0400

    init refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Copyright 2019-2021 Manta Network.
+// This file is part of manta-crypto.
+//
+// manta-crypto is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// manta-crypto is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with manta-crypto.  If not, see <http://www.gnu.org/licenses/>.
+
+mod aux;
+mod checksum;
+mod constants;
+mod dh;
+mod param;
+mod serdes;
+
+pub use aux::*;
+pub use constants::*;
+pub use constants::{COMMIT_PARAM, HASH_PARAM, RECLAIM_PK, TRANSFER_PK};
+pub use param::*;
